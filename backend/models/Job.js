804f795d6764fc7e6060def1444ef27d131da929
Tirumalesh44 <tiru5210@gmail.com>
2025-06-09 22:12:53 +0530

    modification of enum value in models and post job

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -4,7 +4,7 @@ const jobSchema = new mongoose.Schema({
   postedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
-  type: { type: String, enum: ['internship', 'job', 'partTime', 'fullTime'], required: true },
+  type: { type: String, enum: ['internship', 'temporary', 'partTime', 'fullTime'], required: true },
   location: { type: String, required: true },
   duration: { type: String, required: true },
   applyLink: { type: String, required: true },
